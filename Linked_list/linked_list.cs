11349caf9249809def7ffbe3854b9475ecde0bb5
abdelrhman-adel-ahmed <abdelrhman.bedo007@gmail.com>
2021-10-17 00:57:41 +0200

    Update linked_list.cs

--- Linked_list/linked_list.cs
+++ Linked_list/linked_list.cs
@@ -1,12 +1,12 @@
- class Node
+class Node<T>
         {
-        public int data;
-        public Node next;
-        public Node head = null;
+        public T data;
+        public Node<T> next;
+        public Node<T> head = null;
 
-        public void Insert(int data)
+        public void Insert(T data)
         {
-            Node temp = new Node();
+            Node<T> temp = new Node<T>();
             temp.data = data;
             temp.next = null;
             if (head==null)
@@ -15,38 +15,37 @@
                 return;
 
             }
-            Node temp2 = head;
+            Node<T> temp2 = head;
             while(temp2.next!=null)
             {
                 temp2 = temp2.next;
             }
             temp2.next = temp;
         }
-        void recursive_display(ref Node head)
+        void recursive_display(ref Node<T> head)
         {
             if (head == null)
             {
                 return;
             }
-            Console.Write(head.data);
+            Console.WriteLine(head.data);
             recursive_display(ref head.next);
         }
         public void Display()
         {
 
             recursive_display(ref head);
-            Console.WriteLine();
         }
-        public void Delete(int data)
+        public void Delete(T data)
         {
-            if (head.data ==data)
+            if (head.data.Equals(data))
             {
-                Node temp2 = head.next;
+                Node<T> temp2 = head.next;
                 head= temp2;
                 return;
             }
-            Node next_node = head;
-            while (next_node.next != null &&next_node.next.data !=data )
+            Node<T> next_node = head;
+            while (next_node.next != null && !EqualityComparer<T>.Default.Equals(next_node.next.data ,data) )
             {
                 next_node = next_node.next;
             }
@@ -54,14 +53,14 @@
                 next_node.next = next_node.next.next;
         }
 
-        public bool Find(int data)
+        public bool Find(T data)
         {
-            Node temp = head;
-            while (temp.next !=null && temp.data!=data)
+            Node<T> temp = head;
+            while (temp.next !=null && !EqualityComparer<T>.Default.Equals(temp.data,data))
             {
                 temp = temp.next;
             }
-            if (temp.data == data)
+            if (EqualityComparer<T>.Default.Equals(temp.data ,data))
                 return true;
             else
                 return false;
